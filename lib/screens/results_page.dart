e82c891ff0b6b887761390eff1b49eb764d244b0
Choi-Jihyun <osungcjh@naver.com>
2023-04-05 23:56:48 +0900

    complete app 결과 부분에 padding을 추가함

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -2,7 +2,6 @@ import 'package:bmi_calculator_flutter/components/reusable_card.dart';
 import 'package:flutter/material.dart';
 import '../constants.dart';
 import '../components/bottom_button.dart';
-import 'input_page.dart';
 
 class ResultsPage extends StatelessWidget {
   ResultsPage({
@@ -52,9 +51,13 @@ class ResultsPage extends StatelessWidget {
                     bmiResult,
                     style: kBMITextStyle,
                   ),
-                  Text(
-                    interpretation,
-                    style: kBodyTextStyle,
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text(
+                      interpretation,
+                      style: kBodyTextStyle,
+                      textAlign: TextAlign.justify,
+                    ),
                   )
                 ],
               ),
